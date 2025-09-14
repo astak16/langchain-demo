84e5aa37bac36b3614f939407442822f465601ba
uccs <1500846601@qq.com>
2025-09-14 16:41:33 +0800

    ChatMessagePromptTemplate 使用

--- bailian/bailian_tool.py
+++ bailian/bailian_tool.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 from pydantic import SecretStr
 from bailian.env import secret
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, ChatMessagePromptTemplate
 
 
 llm = ChatOpenAI(
@@ -11,10 +11,18 @@ llm = ChatOpenAI(
     streaming=True,
 )
 
+system_message_template = ChatMessagePromptTemplate.from_template(
+    template="你是一个{role}专家、擅长回答{domain}领域的问题", role="system"
+)
+
+human_message_template = ChatMessagePromptTemplate.from_template(
+    template="用户问题：{question}", role="user"
+)
+
 chat_prompt_template = ChatPromptTemplate.from_messages(
     [
-        ("system", "你是一个{role}专家、擅长回答{domain}领域的问题"),
-        ("user", "用户问题：{question}"),
+        system_message_template,
+        human_message_template,
     ]
 )
 prompt = chat_prompt_template.format_messages(
