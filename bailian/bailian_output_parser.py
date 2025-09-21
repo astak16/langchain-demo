f735cf42a7acf7d23b0429176a41a3fc0087e43b
uccs <1500846601@qq.com>
2025-09-21 19:08:27 +0800

    CommaSeparatedListOutputParser 使用

--- bailian/bailian_output_parser.py
+++ bailian/bailian_output_parser.py
@@ -1,16 +1,13 @@
 from bailian.common import llm, chat_prompt_template
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import CommaSeparatedListOutputParser
 
-
-parser = StrOutputParser()
+parser = CommaSeparatedListOutputParser()
 chain = chat_prompt_template | llm | parser
 resp = chain.invoke(
     input={
         "role": "计算",
         "domain": "数学计算",
-        "question": "100+200 = ?",
+        "question": "100*200 = ?",
     }
 )
-
-
 print(resp)
